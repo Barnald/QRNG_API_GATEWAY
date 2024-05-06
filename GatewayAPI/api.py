2b56828e55d4b61fcc52d601fc6edf136c998d4b
Barnald <37440476+Barnald@users.noreply.github.com>
2024-05-06 14:08:12 +0200

    Update api.py

--- GatewayAPI/api.py
+++ GatewayAPI/api.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 import requests
 import uvicorn
-import ecdsa
 from dotenv import load_dotenv 
 import os
 
@@ -9,16 +8,24 @@ app = FastAPI()
 
 @app.get("/int/{size_in_bits}/{max}")
 async def get_random_int(size_in_bits: int, max: int):
+    ''''
+    Function to call a QRNG API provider for random integer.
+        size_in_bits: Integer that represents the size of the number in bits.
+        max: Integer that represents the maximum value for the generation.
+    Returns: JSON object, containing 3 fields:
+        success: Shows if the request was successful -> {true, false}
+        provider: Shows which provider sent the random number
+        data: Contains the random number in decimal   
+    '''
     load_dotenv()
-    anu_url = "https://api.quantumnumbers.anu.edu.au/"
+    anu_url = "https://qrng.anu.edu.au/API/jsonI.php"
     API_KEY = os.getenv("API_KEY")
     ethz_url = "http://qrng.ethz.ch/api/randint"
 
     #Calling ANU API
-    DTYPE = "uint16"  # uint8, uint16, hex8, hex16
-    LENGTH = 1  # between 1--1024
+    DTYPE = "uint8"
 
-    params = {"length": LENGTH, "type": DTYPE}
+    params = {"length": size_in_bits, "type": DTYPE}
     headers = {"x-api-key": API_KEY}
 
     response = requests.get(anu_url, headers=headers, params=params)
@@ -26,11 +33,11 @@ async def get_random_int(size_in_bits: int, max: int):
     backer = 0
     if response.status_code == 200:
         json = response.json()
-        processed_data = []
+        processed_data = ""
         for number in json['data']:
-            processed_data.append(number%(max+1))
-        json['data'] = processed_data
-        return json
+            processed_data += str(number%2)
+        backer = int(processed_data, 2) % max
+        return {"success":"true", "provider":"ANU", "data":backer}
 
     #Calling ETHZ API
     params = {"min": 0, "max": 1, "size":size_in_bits}
@@ -43,29 +50,47 @@ async def get_random_int(size_in_bits: int, max: int):
         for bit in json['result']:
             backer += str(bit)
         backer = int(backer, 2) % max
-        return {'random':backer}
-    return {"error":-1}
+        return {'success':'true', 'provider':'ETHZ', 'data':backer}
+    return {'success':'false', 'provider':'none', 'data':-1}
 
-async def get_random_hex(api_key: str, size_in_bytes: int, quantity: int):
-    anu_url = "https://api.quantumnumbers.anu.edu.au/"
-    ethz_url = "http://qrng.ethz.ch/api/"
+@app.get("/hex/{size_in_bits}/{max}")
+async def get_random_hex(size_in_bits: int, max: int):
+    ''''
+    Function to call a QRNG API provider for random hexadecimal number.
+        size_in_bits: Integer that represents the size of the number in bits.
+        max: Integer that represents the maximum value for the generation.
+    Returns: JSON object, containing 3 fields:
+        success: Shows if the request was successful -> {true, false}
+        provider: Shows which provider sent the random number
+        data: Contains the random number in hexadecimal   
+    '''
+    response = await get_random_int(size_in_bits=size_in_bits, max=max)
+    if response['success'] == 'false':
+        return response
+    
+    number = hex(response['data'])
+    response['data'] = number
+    return response
 
-    DTYPE = "hex16"  # uint8, uint16, hex8, hex16
-    LENGTH = 1  # between 1--1024
-    BLOCKSIZE = 10  # between 1--10. Only needed for hex8 and hex16
+@app.get("/bin/{size_in_bits}/{max}")
+async def get_random_bin(size_in_bits: int, max: int):
+    ''''
+    Function to call a QRNG API provider for random binary number.
+        size_in_bits: Integer that represents the size of the number in bits.
+        max: Integer that represents the maximum value for the generation.
+    Returns: JSON object, containing 3 fields:
+        success: Shows if the request was successful -> {true, false}
+        provider: Shows which provider sent the random number
+        data: Contains the random number in binary   
+    '''
+    response = await get_random_int(size_in_bits=size_in_bits, max=max)
+    if response['success'] == 'false':
+        return response
+    
+    number = bin(response['data'])
+    response['data'] = number
+    return response
 
-    params = {"length": LENGTH, "type": DTYPE, "size": BLOCKSIZE}
-    headers = {"x-api-key": api_key}
-
-    response = requests.get(anu_url, headers=headers, params=params)
-
-    backer = 0
-    if response.status_code == 200:
-        backer = response.json()
-    else:
-        print(f"Error: {response.status_code}")
-
-    return response, int(backer['data'][0], 16)
 
 if __name__ == "__main__":
     uvicorn.run("api:app", port=5000, log_level="info")
